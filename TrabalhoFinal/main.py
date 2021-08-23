a95a203c253334a8e8996de96e25a25cb1ff91d2
alexzarp <alexszds@gmail.com>
2021-08-23 18:40:25 -0300

    Função tfn até o número 702 equivalente a ZZ

--- TrabalhoFinal/main.py
+++ TrabalhoFinal/main.py
@@ -8,12 +8,43 @@ epsilon = 'ε'
 afnd = [['δ']]
 afd = []
 
-# numeracao_minuscula = [("a", 0), ("b", 1), ('d', 2), ('e', 3), ('f', 4), ('g', 5), ('h', 6), ('i', 7), ('j', 8), ('k', 9), ('l', 10), ('m', 11), ('n', 12), ('o', 13), ('p', 14), ('q', 15), ('r', 16), ('s', 17), ('t', 18), ('u', 19), ('v', 20), ('w', 21), ('y', 22), ('k', 23), ('x', 24), ('z', 25)]
+# numeracao_minuscula = [("A", 0), ("b", 1), ("C", 2), ('d', 2), ('e', 3), ('f', 4), ('g', 5), ('h', 6), ('i', 7), ('j', 8), ('k', 9), ('l', 10), ('m', 11), ('n', 12), ('o', 13), ('p', 14), ('q', 15), ('r', 16), ('s', 17), ('t', 18), ('u', 19), ('v', 20), ('w', 21), ('y', 22), ('k', 23), ('x', 24), ('z', 25)]
 # contatos = dict(numeracao_minuscula)
 
 def print_table(vet):
     for i in range(len(vet)):
         print(vet[i])
+# >>> ord('a')
+# 97
+# >>> chr(97)
+# 'a'
+def tfn_26(num): # 10 → 26
+    numb = []
+    if num <= 25:
+        return chr(num + 65)
+
+    num = num - 26
+    numb.append('A')
+    while(True):
+        if (num > 25):
+            numb[0] = ord(numb[0])
+            numb[0]+=1
+            numb[0] = chr(numb[0])
+            num = num - 26
+        elif (num < 0):
+            num = num + 26
+        else:
+            numb.append(chr(num + 65))
+            break
+    
+    numero = ''
+    # print(numb)
+    for i in range(len(numb)):
+        numero = numero + numb[i]
+
+    return numero
+        
+            
 
 # encontra e coloca na tabela os tokens [a-z]
 for i in range (len(arq)):
@@ -40,4 +71,6 @@ for i in range (len(arq)):
 
 # cont = len(afnd[0]) -1
 # for i in range (len(arq)):
-print_table(afnd)
+# print_table(afnd)
+n = int(input("Digite o numero: "))
+print(tfn_26(n))
---
+++ TrabalhoFinal/tfn.py
@@ -0,0 +1,30 @@
+def tfn_26(num): # 10 → 26
+    numb = []
+    if num <= 25:
+        return chr(num + 65)
+
+    num = num - 26
+    numb.append('A')
+    while(True):
+        if (num > 25):
+            numb[0] = ord(numb[0])
+            numb[0]+=1
+            numb[0] = chr(numb[0])
+            num = num - 26
+        elif (num < 0):
+            num = num + 26
+        else:
+            numb.append(chr(num + 65))
+            break
+    
+    numero = ''
+    # print(numb)
+    for i in range(len(numb)):
+        numero = numero + numb[i]
+
+    return numero
+
+i = 0
+while(i < 702):
+    print(tfn_26(i))
+    i+=1
